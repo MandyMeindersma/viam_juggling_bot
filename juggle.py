21145ac250f011ac3abb4dcae01fd47b45dc8b3f
Mandy Meindersma <mandy.m.meindersma@gmail.com>
2026-09-18 19:20:32 -0400

    adding the motion planning with no trajectory

--- juggle.py
+++ juggle.py
@@ -1,4 +1,5 @@
 import asyncio
+import math
 import sys
 
 from viam.components.arm import Arm
@@ -6,9 +7,13 @@ from viam.proto.component.arm import JointPositions
 from viam.services.motion import MotionClient
 from viam.proto.service.motion import MotionConfiguration
 
+from viam.proto.component.arm import MoveOptions
+from viam.proto.service.motion import MoveRequest
 from viam.proto.common import PoseInFrame, Pose
 
 import connect
+
+from google.protobuf.json_format import MessageToJson
 
 
 
@@ -16,50 +21,72 @@ class Juggler:
     def __init__(self, robot):
         self.robot = robot
         self.arm = Arm.from_robot(self.robot, "arm")
+        self.motion = MotionClient.from_robot(self.robot, "builtin")
 
     async def zero(self):
         """Zero out all the joints, returning the arm to a flat, outstretched position."""
-        motion_service = MotionClient.from_robot(self.robot, "builtin")
-        home = PoseInFrame(reference_frame="world", pose=Pose(x=-734.3404649781442, y=0.7709977991432595, z=246.41067435410184, o_x=-0.9998925877347096, o_y=0, o_z=0, theta=174.8205202973959))
+        zero = Pose(x=-608.8575681802733, y=-14.280484044140682, z=-27.480223424094945, o_x=-0.9953762680748285, o_y=-0.001203186670059822, o_z=0.09604497537749951, theta=176.1478741307629)
+
+        req = MoveRequest(
+            name="builtin",
+            component_name="arm",
+            destination=PoseInFrame(reference_frame="world", pose=zero),
+            world_state=None,
+            constraints=None,
+        )
+        resp = await self.motion.do_command({"plan":MessageToJson(req)})
+        trajectory = resp["plan"]
+
+        positions = [
+            JointPositions(values=[math.degrees(v) for v in step["arm"]])
+            for step in trajectory
+            if step.get("arm")
+        ]
+        options = MoveOptions(
+            max_acc_degs_per_sec2=90,
+            max_vel_degs_per_sec=90
+        )
+
+        await self.arm.move_through_joint_positions(positions, options)
 
         
-        # Define your speed constraints
-        speed_config = MotionConfiguration(
-            linear_m_per_sec=0.2,       # Sets the maximum linear velocity
-            angular_degs_per_sec=90   # Sets the maximum turning/joint velocity
-        )
-
-        # Move the arm using the constraints
-        await motion_service.move(
-            component_name="arm",
-            destination=home,
-            motion_configuration=speed_config
-        )
-
-    async def high(self):
-        """Zero out all the joints, returning the arm to a flat, outstretched position."""
-        motion_service = MotionClient.from_robot(self.robot, "builtin")
-        home = PoseInFrame(reference_frame="world", pose=Pose(x=-620.8801241253041, y=13.065989629810097, z=564.3014151250942, o_x=-0.9994030742493492, o_y=0.016881295816552816, o_z=0.03014161628884865, theta=174.6887362033622))
-
-        
-        # Define your speed constraints
-        speed_config = MotionConfiguration(
-            linear_m_per_sec=0.2,       # Sets the maximum linear velocity
-            angular_degs_per_sec=90   # Sets the maximum turning/joint velocity
-        )
-
-        # Move the arm using the constraints
-        await motion_service.move(
-            component_name="arm",
-            destination=home,
-            motion_configuration=speed_config
-        )
 
     async def throw(self):
-        """Throw the ball"""
-        await self.arm.move_to_joint_positions(
-            JointPositions(values=[5.0, -38.4, 4.3, 0.7, -64.5, 0.4])
+        """Zero out all the joints, returning the arm to a flat, outstretched position."""
+        # pose = PoseInFrame(reference_frame="world", pose=Pose(x=-620.8801241253041, y=13.065989629810097, z=564.3014151250942, o_x=-0.9994030742493492, o_y=0.016881295816552816, o_z=0.03014161628884865, theta=174.6887362033622))
+        pose = PoseInFrame(reference_frame="world", pose=Pose(x=-690.7447881801012,y=11.93337068601625,z=431.2269891028385,o_x=-0.9997518095214151,o_y=.00631436298081086,o_z=.021364647874532516,theta=175.78125152270124))
+
+        req = MoveRequest(
+            name="builtin",
+            component_name="arm",
+            destination=pose,
+            world_state=None,
+            constraints=None
         )
+        resp = await self.motion.do_command({"plan":MessageToJson(req)})
+        trajectory = resp["plan"]
+
+        positions = [
+            JointPositions(values=[math.degrees(v) for v in step["arm"]])
+            for step in trajectory
+            if step.get("arm")
+        ]
+        
+        await self.arm.move_through_joint_positions(
+            positions,
+            options= MoveOptions(
+                max_acc_degs_per_sec2=1100,
+                max_vel_degs_per_sec=180
+            )
+        )
+
+        
+
+    # async def throw(self):
+    #     """Throw the ball"""
+    #     await self.arm.move_to_joint_positions(
+    #         JointPositions(values=[5.0, -38.4, 4.3, 0.7, -64.5, 0.4])
+    #     )
 
 
 
@@ -71,7 +98,6 @@ class Juggler:
 STEPS = {
     "throw": Juggler.throw,
     "zero": Juggler.zero,
-    "high": Juggler.high,
 }
 
 
